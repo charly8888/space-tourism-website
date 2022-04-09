271547be1c86c5166a27fe0f2c4d803542306798
German Hornus <hornusgerman@gmail.com>
2022-04-09 17:31:44 -0300

    remove background images of body

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,14 +2,13 @@
 import { Link } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import TextHome from "./components/TextHome"
-import css from "./homepage.module.sass"
 
 const HomePage = () => {
-  return <main className={css.main}>
+  return <>
     <Navbar />
     <TextHome />
     <Link to="/" className="explore">EXPLORE</Link>
-  </main>
+  </>
   
 }
 
