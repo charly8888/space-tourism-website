2a44d67b4145648dc46d773bdfa3b3c422d29ee5
German Hornus <hornusgerman@gmail.com>
2022-04-10 17:29:48 -0300

    add global width state for navbar right now

---
+++ hooks/useWidth.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react"
+
+export const useWidth = () => {
+  const [width, setWidth] = useState(window.innerWidth)
+  useEffect(() => {
+    window.addEventListener("resize", () => 
+    setWidth(window.innerWidth)
+    )
+    return () => {
+      window.removeEventListener("resize", () => 
+    setWidth(window.innerWidth)
+    )
+    }
+  }, [])
+  
+  return (
+    width
+  )
+}
