bf9742d2555a2bc91221b22d535d369395a788ff
German Hornus <hornusgerman@gmail.com>
2022-04-10 19:56:59 -0300

    add diferents color navbar for chorme and firefox

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,13 +15,13 @@ const Navbar:FC = () => {
     e.stopPropagation();
     setNavbar(!navbar);
   }
-
+  const colorForMozilla =  () => navigator.userAgent.search("Firefox") && "rgba(0, 0, 0, 0.8)" 
   if (useWidth() <= 376) {
     return<>
         <img src="../../assets/shared/logo.svg" alt="logo" className={css.img}/>
         { navbar ? (
         <header className={css.header} onClick={()=>setNavbar(navbar?!navbar:navbar)}>
-          <nav className={`${css.nav} ${css[openOrNot]}`}>
+          <nav style={{backgroundColor:`${colorForMozilla()}` }} className={`${css.nav} ${css[openOrNot]}` }>
             <span className={css.span} onClick={handleClickStopPropagation }></span>
               <NavLink  to="/" className="navlink" onClick={handleClickStopPropagation } ><span>00</span> Home</NavLink>
               <NavLink  to="/destination"  className="navlink" onClick={handleClickStopPropagation }><span>01</span> Destination</NavLink>
@@ -34,7 +34,7 @@ const Navbar:FC = () => {
       </>
   }else{
     return<>
-      <header className={css.header} onClick={()=>setNavbar(navbar?!navbar:navbar)}>
+      <header className={css.header}  onClick={()=>setNavbar(navbar?!navbar:navbar) }>
           <img src="../../assets/shared/logo.svg" alt="logo" className={css.img}/>
           <nav className={`${css.nav} ${css[openOrNot]}`}>
             <span className={css.span} onClick={handleClickStopPropagation }></span>
