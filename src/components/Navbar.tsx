5f8e9d1ec1a1db0a69fce6735cd8c05bd8fa4610
German Hornus <hornusgerman@gmail.com>
2022-04-10 11:33:15 -0300

    final design navbar mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,41 +4,26 @@ import css from './navbar.module.sass'
 const Navbar = () => {
   const [state, setState] = useState(false)
   const openOrNot = state ? 'open' : 'close'
-  console.log(state) 
-  const [width, setWidth] = useState(window.innerWidth);
-  useEffect(() => {
-   const handleResizeWindow = () => setWidth(window.innerWidth);
-    // subscribe to window resize event "onComponentDidMount"
-    window.addEventListener("resize", handleResizeWindow);
-    return () => {
-      // unsubscribe "onComponentDestroy"
-      window.removeEventListener("resize", handleResizeWindow);
-    };
-  }, []);
-console.log(width)
-
-if (width < 375) {
-  return (<>
-      <img src="../../assets/shared/logo.svg" alt="logo" className={css.img}/>
-      <nav className={`${css.nav} ${css[openOrNot]}`}>
-          <NavLink  to="/" className="navlink" ><span>00</span> Home</NavLink>
-          <NavLink  to="/destination"  className="navlink" ><span>01</span> Destination</NavLink>
-          <NavLink  to="/crew" className="navlink"><span>02</span>  Crew</NavLink>
-          <NavLink  to="/tech" className="navlink"><span>03</span> Technology</NavLink>
-      </nav>
-    <button className={`${css.button} ${css[state ? "cruz" : ""]}`} onClick={() => setState(!state)}></button>
-  </>
-  )
+  console.log(state)
+const handleClickSpan= (e) => {
+  e.stopPropagation();
+}
+const handleClick = (e) => {
+  e.stopPropagation();
+  setState(!state);
 }
   return (<>
-    <header className={css.header} >
+    <header className={css.header} onClick={()=>setState(state?!state:state)}>
       <img src="../../assets/shared/logo.svg" alt="logo" className={css.img}/>
-      <nav className={css.nav}>
+      <nav className={`${css.nav} ${css[openOrNot]}`}>
+        <span className={css.span} onClick={handleClickSpan}></span>
           <NavLink  to="/" className="navlink" ><span>00</span> Home</NavLink>
           <NavLink  to="/destination"  className="navlink" ><span>01</span> Destination</NavLink>
           <NavLink  to="/crew" className="navlink"><span>02</span>  Crew</NavLink>
           <NavLink  to="/tech" className="navlink"><span>03</span> Technology</NavLink>
+        <span className={css.span} onClick={handleClickSpan}></span>
       </nav>
+    <button className={`${css.button} ${css[state ? "cruz" : ""]}`} onClick={handleClick}></button>
     </header>
   </>
   )
